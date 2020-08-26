22a9a65d52c0ec4001780004239640a13e973dc0
Apergot <apergot95@gmail.com>
2020-08-26 20:46:21 +0100

    conf mongo connection and adding three roles to roles collection

--- server.js
+++ server.js
@@ -5,9 +5,28 @@ const cors = require('cors')
 const app = express();
 
 var corsOptions = {
-    origin : "http://localhost:8081"
+    origin : 'http://localhost:8081'
 };
 app.use(cors(corsOptions))
+
+const db = require('./app/models');
+const dbConfig = require('./app/config/db.config');
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Success connecting to mongodb')
+        // aux function used to create three rows in roles collection
+        initial();
+    })
+    .catch(err => {
+        console.error('Connection error', err);
+        process.exit();
+    });
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -25,3 +44,39 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 });
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'user' to roles collection");
+        });
+  
+        new Role({
+          name: "mod"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'mod' to roles collection");
+        });
+  
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
